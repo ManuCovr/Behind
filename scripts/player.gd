89ebebcf4c040e2b8b4ca7837ea3b9150e84f89b
ManuCovr <mtmpfb@gmail.com>
2024-11-28 07:50:54 +0000

    JUMP BUFFER!!!!!

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
+@export var jump_buffer_buffer : float = 0.1 
 @export var SPEED : float = 100.0
 @export var JUMP_VELOCITY : float = -160.0
 @export var double_jump_velocity : float = -130
 @onready var marker_2d: Marker2D = $Marker2D
 @onready var sprite: AnimatedSprite2D = $Marker2D/AnimatedSprite2D
+@onready var jump_buffer_timer: Timer = $JumpBufferTimer
 
+var jump_buffer : bool = false
 var has_double_jumped : bool = false
 var animation_locked: bool = false
 var direction : Vector2 = Vector2.ZERO
@@ -16,14 +19,9 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	else:
 		has_double_jumped = false
-
-	# Handle jump.
 	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = JUMP_VELOCITY
-		elif not has_double_jumped:
-			velocity.y = double_jump_velocity
-			has_double_jumped = true
+		jump()
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -33,9 +31,32 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
+	var was_on_floor = is_on_floor()
 	move_and_slide()
 	update_animation()
 	update_facing_direction()
+	
+	#touched ground
+	if !was_on_floor && is_on_floor():
+		if jump_buffer:
+			jump_buffer = false
+			jump()
+
+func jump():
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif not has_double_jumped:
+			velocity.y = double_jump_velocity
+			has_double_jumped = true
+		#buffer jump
+		else:
+			if !jump_buffer:
+				jump_buffer = true
+				jump_buffer_timer.start()
+
+
+func _on_jump_buffer_timer_timeout():
+	jump_buffer = false
 
 func update_animation():
 	if not animation_locked:
@@ -50,6 +71,3 @@ func update_facing_direction():
 	if direction.x < 0:
 		marker_2d.scale.x=-1
 		
-			
-			
-			
